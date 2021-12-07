e0e647a320ec353e6bf3e52b5767a1a881dc0fb5
Elton Andrade <eltonfernandesandrade@hotmail.com>
2021-12-07 13:49:58 -0300

    Desafio do primeiro dia, parte 1

---
+++ Dia01-varredura-do-sonar/Classes/Desafio.cs
@@ -0,0 +1,7 @@
+namespace Dia01_varredura_do_sonar.Classes
+{
+  public abstract class Desafio
+  {
+    public abstract int Resolver();
+  }
+}
---
+++ Dia01-varredura-do-sonar/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Dia01_varredura_do_sonar.Classes;
+
+namespace Dia01_varredura_do_sonar
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Desafio desafio = new Parte1();
+      var resultado = desafio.Resolver();
+
+      Console.WriteLine(resultado);
+    }
+  }
+}
