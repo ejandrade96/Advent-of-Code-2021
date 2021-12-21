abd01c8fc4e0f2689f3842b44408e993f37e3a17
Elton Andrade <eltonfernandesandrade@hotmail.com>
2021-12-21 13:27:49 -0300

    Criando projeto do dia 06

---
+++ Dia06-lanternfish/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Dia06_lanternfish
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
